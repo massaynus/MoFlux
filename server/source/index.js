59f0f4370e590c3aa545f5c099a2b07391b0aaaa
Yassine Elmassaoudi <yassine.elmassaoudi@imedia24.de>
2022-09-25 23:46:58 +0100

    add socket.io

--- server/source/index.js
+++ server/source/index.js
@@ -1,38 +1,52 @@
-import express from 'express';
-import cors from 'cors';
-import { connect } from './lib/mongoose';
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import uuid from "uuid";
 
-import { SERVER_PORT, ENV } from './lib/config'
-import API_ROUTER_V1 from './routes/api_v1'
-import Auth from './middlewares/Auth'
-import ResponseBodyInterceptor from './middlewares/ResponseBodyInterceptor'
+import { connect } from "./lib/mongoose";
+import { SERVER_PORT, ENV } from "./lib/config";
+import API_ROUTER_V1 from "./routes/api_v1";
+import Auth from "./middlewares/Auth";
+import ResponseBodyInterceptor from "./middlewares/ResponseBodyInterceptor";
 
-const startUpTime = new Date()
+const startUpTime = new Date();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(ResponseBodyInterceptor)
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(ResponseBodyInterceptor);
 
-app.use('/api/v1', Auth, API_ROUTER_V1)
+app.use("/api/v1", Auth, API_ROUTER_V1);
 
-app.get('/heartbeat', (_, res) => {
-    const beat = { startUpTime }
-    console.log('I am alive')
-    res.json(beat)
-})
+app.get("/heartbeat", (_, res) => {
+  const beat = { startUpTime };
+  console.log("I am alive");
+  res.json(beat);
+});
 
-app.use(function (req, res, next, err) {
-    if (!err) res.end()
-    else {
-        console.log('error:', err)
-        res.status(500).json(ENV === "DEVELOPMENT" ? err : { message: "NO STACK IN PROD", params: req.params, body: req.body })
-    }
-})
+app.use(function (req, res, _, err) {
+  if (!err) res.end();
+  else {
+    console.log("error:", err);
+    res
+      .status(500)
+      .json(
+        ENV === "DEVELOPMENT"
+          ? err
+          : { message: "NO STACK IN PROD", params: req.params, body: req.body }
+      );
+  }
+});
 
-main()
+const httpServer = createServer(app);
+const io = new Server(httpServer, { path: "/ws" });
+
+io.main();
 
 async function main() {
-    await connect()
-    app.listen(SERVER_PORT, () => console.log(`Server in "${ENV}" mode & listening on ${SERVER_PORT}!`));
+  await connect();
+  httpServer.listen(SERVER_PORT, () =>
+    console.log(`Server in "${ENV}" mode & listening on ${SERVER_PORT}!`)
+  );
 }
